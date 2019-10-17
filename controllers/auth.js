c2851be33d1721b80b26e3e366c74f6fe4ac45c5
cerebrium <nicholas.m.shankland@gmail.com>
2019-10-17 09:08:52 -0700

    day 2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models')
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
+
+router.post(`/signup`, function(req, res) {
+  // find or create user
+  db.user.findOrCreate({
+    where : {
+      email: req.body.email
+    }, 
+    defaults: {
+      name : req.body.name,
+      password: req.body.password
+    }
+  }).then(function([user, created]){
+    created ? res.redirect('/') : res.redirect('/auth/signup');
+  }).catch(function(err) {
+    console.log(err)
+    res.redirect('/auth/signup')
+  })
+  // if user existed err and redirect to signup
+})
 
 router.get('/login', function(req, res) {
   res.render('auth/login');
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const passport = require('./config/ppCinfig')
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +12,15 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(ejsLayouts);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res) {
   res.render('index');
