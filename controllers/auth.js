846e5214041e852aa783ec6794ef7e6928f0e2e4
cerebrium <nicholas.m.shankland@gmail.com>
2019-10-17 10:32:30 -0700

    did some flash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models')
+const db = require('../models');
+const passport = require('../config/ppCinfig')
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
@@ -17,7 +18,15 @@ router.post(`/signup`, function(req, res) {
       password: req.body.password
     }
   }).then(function([user, created]){
-    created ? res.redirect('/') : res.redirect('/auth/signup');
+    if (created) {
+      passport.authenticate('local',  {
+        successRedirect: '/', 
+        successFlash: 'User logged in'
+      })(req, res)
+    } else {
+      req.flash('error', 'Email already exists')
+      res.redirect('/auth/signup')
+    }
   }).catch(function(err) {
     console.log(err)
     res.redirect('/auth/signup')
@@ -28,5 +37,18 @@ router.post(`/signup`, function(req, res) {
 router.get('/login', function(req, res) {
   res.render('auth/login');
 });
+
+router.post(`/login`, passport.authenticate('local', {
+  successRedirect: '/',
+  successFlash: 'You have logged in!',
+  failureRedirect: '/auth/login',
+  failureFlash: 'Invalid username or password'
+}))
+
+router.get(`/logout`, function(req, res) {
+  req.logout();
+  req.flash('success', 'you have been logged out!')
+  res.redirect('/');
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const session = require('express-session');
-const passport = require('./config/ppCinfig')
+const passport = require('./config/ppCinfig');
+const flash = require('connect-flash');
 
+// middleware
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
@@ -13,15 +15,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(ejsLayouts);
 
+// session must come before flash and passport
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
+// must come after session but before passport
+app.use(flash());
 
+// passport initializaiton
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// middleware ended, app getting
 app.get('/', function(req, res) {
   res.render('index');
 });
